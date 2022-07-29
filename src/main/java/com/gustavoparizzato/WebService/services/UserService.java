7f370cb8e0cbe53a32c5e1503df6a7619e9eb192
Gustavo Parizzato <86897255+gustavoparizzato@users.noreply.github.com>
2022-07-29 17:48:10 -0300

    Exception handling - findById

--- src/main/java/com/gustavoparizzato/WebService/services/UserService.java
+++ src/main/java/com/gustavoparizzato/WebService/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gustavoparizzato.WebService.entities.User;
 import com.gustavoparizzato.WebService.repositories.UserRepository;
+import com.gustavoparizzato.WebService.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
