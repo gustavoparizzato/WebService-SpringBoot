709c3231ae13606a223dafaaf986fbb5151b54c3
Gustavo Parizzato <86897255+gustavoparizzato@users.noreply.github.com>
2022-08-01 15:45:08 -0300

    Exception handling - delete

--- src/main/java/com/gustavoparizzato/WebService/services/UserService.java
+++ src/main/java/com/gustavoparizzato/WebService/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gustavoparizzato.WebService.entities.User;
 import com.gustavoparizzato.WebService.repositories.UserRepository;
+import com.gustavoparizzato.WebService.services.exceptions.DatabaseException;
 import com.gustavoparizzato.WebService.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
